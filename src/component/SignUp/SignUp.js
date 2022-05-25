4683f953ce60b1ce1711334a24362e2946ba0e6a
shohayabt <shohayabhasan@yahoo.com>
2022-05-25 14:47:52 +0600

    signup complete with email and google both

--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,15 +1,48 @@
 import React from "react";
 import { FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import auth from "../../firebase";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSendEmailVerification,
+  useSignInWithGoogle,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
 
 const SignUp = () => {
+  // UPDATING PROFILE
+  const [displayName, setDisplayName] = useState("");
+  const [updateProfile, updating, updateProfileError] = useUpdateProfile(auth);
+
+  // SIGN IN WITH GOOGLE
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  // SIGN IN WITH EMAIL AND PASSWORD
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  // SEND  EMAIL VERIFICATION
+  const [sendEmailVerification, sending, verificationError] =
+    useSendEmailVerification(auth);
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="card w-96 bg-base-100 shadow-xl px-8 py-5">
         <h1 className="login-title text-4xl leading-15 mb-5 font-bold">
           Sign Up
         </h1>
-        <div className="form-control  w-full max-w-xs">
+        <form
+          className="form-control  w-full max-w-xs"
+          onSubmit={async (event) => {
+            event.preventDefault();
+            createUserWithEmailAndPassword(email, password);
+            await updateProfile({ displayName });
+            await sendEmailVerification(email);
+          }}
+        >
           <label className="label">
             <span className="label-text">ENTER YOUR NAME:</span>
           </label>
@@ -17,6 +50,7 @@ const SignUp = () => {
             type="text"
             placeholder="Shohayab Hasan Talha"
             className="input input-bordered w-full max-w-xs"
+            onChange={(e) => setDisplayName(e.target.value)}
           />
           <label className="label">
             <span className="label-text">ENTER YOUR EMAIL:</span>
@@ -25,6 +59,7 @@ const SignUp = () => {
             type="email"
             placeholder="info@example.com"
             className="input input-bordered w-full max-w-xs"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label className="label">
             <span className="label-text-alt"></span>
@@ -36,13 +71,17 @@ const SignUp = () => {
             type="password"
             placeholder="password"
             className="input input-bordered w-full max-w-xs"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label className="label">
             <span className="label-text-alt"></span>
           </label>
           <button className="w-100 btn-full">SignUp</button>
           <div className="divider">OR</div>
-          <button className="btn btn-active w-100 btn-secondary">
+          <button
+            className="btn btn-active w-100 btn-secondary"
+            onClick={async () => await signInWithGoogle()}
+          >
             <FaGoogle className="mr-2 text-lg"></FaGoogle>
             SignUp with google
           </button>
@@ -53,7 +92,7 @@ const SignUp = () => {
               LOGIN
             </Link>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
