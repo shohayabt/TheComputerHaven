8b1a613f676d54000c6f5a1c9836630318f5ed84
shohayabt <shohayabhasan@yahoo.com>
2022-05-27 21:39:23 +0600

    product card added

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Card.css";
 
 const Card = (props) => {
+  const { description, _id, price, quantity, name, imageUrl } = props.product;
   const navigate = useNavigate();
   const navigateToPurchase = (id) => {
-    navigate(`/car/${id}`);
+    navigate(`/product/${id}`);
   };
 
   return (
     <div>
       <div className="card w-96 glass">
         <figure>
-          <img src="https://api.lorem.space/image/car?w=400&h=225" alt="car!" />
+          <img src={imageUrl} alt="car!" />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">Life hack</h2>
-          <span className="flex justify-between">
+        <div className="card-body px-10">
+          <h2 className="card-title">{name}</h2>
+          <span className="price">
             <span className="text-red-600">
               <label class="input-group">
                 <span>Price</span>
@@ -23,25 +25,26 @@ const Card = (props) => {
                   type="text"
                   disabled
                   readOnly
-                  value={10}
+                  value={price}
                   class="input-md input-bordered w-30"
                 />
               </label>
             </span>
           </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto cum,
-            nesciunt sit saepe debitis ad autem dolores expedita officia ducimus
-            aliquam a error doloremque, voluptates eaque rem magni porro. Ullam.
+          <p className="p">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+            facere id sapiente quisquam sequi amet consequatur debitis unde
+            corporis, impedit animi magnam in ipsum deserunt, officiis aut.
+            Amet, cum nostrum.
           </p>
-          <span className="text-red-600 w-50">
+          <span className="text-red-600 w-50 price">
             <label class="input-group">
               <span>Quantity</span>
               <input
                 type="text"
                 disabled
                 readOnly
-                value={10}
+                value={quantity}
                 class="input-md input-bordered w-30"
               />
             </label>
@@ -50,7 +53,7 @@ const Card = (props) => {
             <button
               className="btn-full signout"
               onClick={() => {
-                navigateToPurchase();
+                navigateToPurchase(_id);
               }}
             >
               purchase now
