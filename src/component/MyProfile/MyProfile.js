0b161026d13f835bf1513ecd2434812bcc819cb5
shohayabt <shohayabhasan@yahoo.com>
2022-05-30 01:51:46 +0600

    update profile

--- src/component/MyProfile/MyProfile.js
+++ src/component/MyProfile/MyProfile.js
@@ -20,7 +20,7 @@ const MyProfile = () => {
             number,
             address,
           };
-          const url = `https://polar-taiga-99861.herokuapp.com/updateUser/${email}`;
+          const url = `http://localhost:5000/updateUser/${email}`;
           fetch(url, {
             method: "PUT",
             body: JSON.stringify(updateUser),
