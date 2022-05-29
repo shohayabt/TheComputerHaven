52282e8985e84d5ed5308b39a1536871d42cd284
shohayabt <shohayabhasan@yahoo.com>
2022-05-30 00:54:07 +0600

    footer section updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,27 +1,65 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase";
 
 const Footer = () => {
+  const [user] = useAuthState(auth);
   return (
-    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <footer className="footer px-10 py-12 bg-neutral text-neutral-content">
       <div>
         <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <Link to="/" className="link link-hover uppercase">
+          Branding
+        </Link>
+        <Link to="/" className="link link-hover uppercase">
+          Design
+        </Link>
+        <Link to="/" className="link link-hover uppercase">
+          Marketing
+        </Link>
+        <Link to="/" className="link link-hover uppercase">
+          Advertisement
+        </Link>
       </div>
       <div>
         <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <Link to="/blog" className="link link-hover uppercase">
+          BLOG
+        </Link>
+        <Link to="/myPortfolio" className="link link-hover uppercase">
+          PORTFOLIO
+        </Link>
+        <Link to="/dashboard" className="link link-hover uppercase">
+          DASHBOARD
+        </Link>
+        <Link to="/" className="link link-hover uppercase">
+          {user && (
+            <button
+              style={{ margin: "0px" }}
+              className="btn-full signout mx-0"
+              onClick={() => {
+                signOut(auth);
+                localStorage.removeItem("accessToken");
+              }}
+            >
+              LOG OUT
+            </button>
+          )}
+        </Link>
       </div>
       <div>
         <span className="footer-title">Legal</span>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <Link to="/" className="link link-hover uppercase">
+          Terms of use
+        </Link>
+        <Link to="/" className="link link-hover uppercase">
+          Privacy policy
+        </Link>
+        <Link to="/" className="link link-hover uppercase">
+          Cookie policy
+        </Link>
       </div>
     </footer>
   );
